5310edb1711505e8f37a946aabb959a9ee2bc421
sanyam raj <sanyamraj007@gmail.com>
2023-06-10 21:36:02 +0530

    first try

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
-  
+    public static class A {
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A{
+        @Override
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+    public static void main(String[] args) {
+        B e = new B();
+        e.meth();
+
+        B e1 = new B();
+        e1.meth();
+    }
 }
